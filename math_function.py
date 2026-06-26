c6fa79b1b76652a239b5cadc0ba783bdd02f37f9
Aesha Prajapati <aeshaprajapati89@gmail.com>
2026-06-26 20:45:07 +0530

    Python math module
    - Added examples of Python math module functions
    - Demonstrated mathematical constants (pi and e)
    - Implemented ceil(), floor(), and fabs()
    - Added remainder(), pow(), and sqrt() examples
    - Included modf() and factorial() demonstrations
    - Showcased common mathematical operations using the math module

---
+++ math_function.py
@@ -0,0 +1,64 @@
+import math
+print("PI value is :",math.pi)
+print("Euler value is :",math.e)
+
+print("\n")
+
+n1 = 10.55
+n2 = 11.99
+n3 = 9
+
+print("Ceil value is :",math.ceil(n1))
+print("Ceil value is :",math.ceil(n2))
+print("Ceil value is :",math.ceil(n3))
+print("Floor value is :",math.floor(n1))
+print("Floor value is :",math.floor(n2))
+print("Floor value is :",math.floor(n3))
+
+print("\n")
+
+n1 = -10.55
+n2 = -11
+n3 = 9
+print("Absolute value is :",math.fabs(n1))
+print("Absolute value is :",math.fabs(n2))
+print("Absolute value is :",math.fabs(n3))
+
+print("\n")
+
+print("Remainder value is :")
+n1 = math.remainder(10,2)
+n2 = math.remainder(10,3)
+n3 = math.remainder(14,8)
+
+print("N1 value is :",n1)
+print("N2 value is :",n2)
+print("N3 value is :",n3)
+
+print("\n")
+
+n1 = math.pow(2,2)
+n1 = math.pow(2,4)
+
+print("N1 value is :",n1)
+print("N2 value is :",n2)
+
+print("\n")
+
+n1 = math.sqrt(16)
+n1 = math.sqrt(60)
+
+print("N1 value is :",n1)
+print("N2 value is :",n2)
+
+print("\n")
+
+n1 = math.modf(16)
+print("N1 value is :",n1)
+
+print("\n")
+
+n1 = math.factorial(5)
+print("N1 value is :",n1)
+
+print("\n")
